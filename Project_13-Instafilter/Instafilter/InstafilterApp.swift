72fb699cc5c20408015b7db4a1b1fa65fef86575
Georgi Ignatov <75448563+gignatov@users.noreply.github.com>
2025-09-18 16:00:12 +0300

    Add Project 13 - Instafilter

---
+++ Project_13-Instafilter/Instafilter/InstafilterApp.swift
@@ -0,0 +1,17 @@
+//
+//  InstafilterApp.swift
+//  Instafilter
+//
+//  Created by Georgi Ignatov on 17.07.25.
+//
+
+import SwiftUI
+
+@main
+struct InstafilterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
