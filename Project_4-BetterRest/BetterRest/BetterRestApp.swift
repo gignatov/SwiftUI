9db7a09db0c006dba28204ed9a38b1f5940b0f2e
Georgi Ignatov <75448563+gignatov@users.noreply.github.com>
2025-09-18 15:15:02 +0300

    Add Project 4 - BetterRest

---
+++ Project_4-BetterRest/BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Georgi Ignatov on 13.05.25.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
