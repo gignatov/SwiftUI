af8bdd0d39951bf866d5c972d826fed0cdbf4f3c
Georgi Ignatov <75448563+gignatov@users.noreply.github.com>
2025-09-18 15:05:54 +0300

    Add Project 1 - WeSplit

---
+++ Project_1-WeSplit/WeSplit/WeSplitApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeSplitApp.swift
+//  WeSplit
+//
+//  Created by Georgi Ignatov on 16.01.25.
+//
+
+import SwiftUI
+
+@main
+struct WeSplitApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
